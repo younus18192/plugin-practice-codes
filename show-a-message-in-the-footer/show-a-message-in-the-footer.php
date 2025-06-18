68ad510612371c68e5b792ceb4c04d25c85d7cca
younus18192 <younus.18192@gmail.com>
2025-06-18 14:45:39 +0500

    Delete show-a-message-in-the-footer directory

--- show-a-message-in-the-footer/show-a-message-in-the-footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * Plugin Name: show a message in the footer
- */
-function samitf_function() {
-    echo '<center><p><b>This is a custom text inserted in the footer</b></p></center>';
-}
-add_action( 'wp_footer', 'samitf_function' );
-?>
